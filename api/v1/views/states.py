33fdb6f0e6d4974d7bf6d7d631c576283ccf5491
Hamabarhamou <hama.peinture.barhamou@gmail.com>
2022-10-23 13:54:21 +0100

    raise a 404

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -6,11 +6,16 @@ states api
 from models import storage
 from models.state import State
 from api.v1.views import app_views_states
-from flask import jsonify, request
+from flask import jsonify, request, redirect
+from werkzeug.exceptions import HTTPException
 
-@app_views_states.route('/states', strict_slashes=False, 
+@app_views_states.route('/states',
+                        strict_slashes=False, 
+                        methods = ['GET'])
+@app_views_states.route('/states/<state_id>',
+                        strict_slashes=False, 
                         methods = ['POST', 'GET'])
-def states():
+def states(state_id=None,):
     if request.method == 'GET':
         reponse = storage.all(State).values()
         result = []
@@ -23,8 +28,13 @@ def states():
             dic['name'] = r['name']
             dic['updated_at'] = r['updated_at']
             result.append(dic)
-        for loop in result:
-            print(loop)
+
+        if state_id is not None:
+            for loop in result:
+                if state_id == loop['id']:
+                    return loop
+            return jsonify({"error": "Not found"}), '404'
+
         return result
     
     if request.method == 'POST':
