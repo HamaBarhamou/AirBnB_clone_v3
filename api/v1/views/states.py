054a23daa2b78ee2e37c8a78fb735802be6fb709
Hamabarhamou <hama.peinture.barhamou@gmail.com>
2022-10-23 12:20:41 +0100

    begining implementation a the task 6. State

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -6,8 +6,13 @@ states api
 from models import storage
 from models.state import State
 from api.v1.views import app_views_states
-from flask import jsonify
+from flask import jsonify, request
 
-@app_views_states.route('/states/', strict_slashes=False)
+@app_views_states.route('/states/', strict_slashes=False, 
+                        methods = ['POST', 'GET'])
 def states():
-    return jsonify({"states": "listes of the states"})
+    if request.method == 'GET':
+        return jsonify({"states": "GET methode"})
+    
+    if request.method == 'POST':
+        return jsonify({"states": "POST methode"})
