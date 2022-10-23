cac7850661716a36d3a3cc93138e0274c0596be2
Hamabarhamou <hama.peinture.barhamou@gmail.com>
2022-10-23 13:17:06 +0100

    api/v1/views/states.py

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -8,11 +8,24 @@ from models.state import State
 from api.v1.views import app_views_states
 from flask import jsonify, request
 
-@app_views_states.route('/states/', strict_slashes=False, 
+@app_views_states.route('/states', strict_slashes=False, 
                         methods = ['POST', 'GET'])
 def states():
     if request.method == 'GET':
-        return jsonify({"states": "GET methode"})
+        reponse = storage.all(State).values()
+        result = []
+        for loop in reponse:
+            r = loop.to_dict()
+            dic = {}
+            dic['__class__'] = r['__class__']
+            dic['created_at'] = r['created_at']
+            dic['id'] = r['id']
+            dic['name'] = r['name']
+            dic['updated_at'] = r['updated_at']
+            result.append(dic)
+        for loop in result:
+            print(loop)
+        return result
     
     if request.method == 'POST':
         return jsonify({"states": "POST methode"})
