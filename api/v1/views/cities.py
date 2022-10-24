f068cececce877764d99b3ffb6fc1d55e4eaad1e
Hamabarhamou <hama.peinture.barhamou@gmail.com>
2022-10-24 12:15:49 +0100

    struturation cities.py file

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -9,6 +9,8 @@ from models.city import City
 from api.v1.views import app_views
 from flask import jsonify, request, redirect
 from werkzeug.exceptions import HTTPException
+import requests
+from os import getenv
 
 
 @app_views.route('/states/<state_id>/cities',
@@ -18,5 +20,10 @@ from werkzeug.exceptions import HTTPException
                  strict_slashes=False,
                  methods=['GET', 'DELETE', 'PUT'])
 def Cities(state_id=None, city_id=None):
+    HBNB_API_HOST = getenv('HBNB_API_HOST')
     if request.method == 'GET':
+        """x = requests.get('https://w3schools.com/python/demopage.htm')
+        x = requests.get('http://'+HBNB_API_HOST+'/api/v1/states/{}'
+                         .format(state_id))
+        print("status code:",x.status_code)"""
         return jsonify({"msg": "Cities List all"}), 200
