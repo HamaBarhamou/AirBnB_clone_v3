cacd860a7b3ecb75be06a730460dd7876db7b867
Hamabarhamou <hama.peinture.barhamou@gmail.com>
2022-10-25 14:46:30 +0100

    Retrieves a City object. : GET /api/v1/cities/<city_id>

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -13,41 +13,6 @@ from flask import jsonify, abort
 from werkzeug.exceptions import HTTPException
 
 
-"""@app_views.route('/states/<state_id>/cities',
-                 strict_slashes=False,
-                 methods=['GET', 'DELETE', 'POST'])
-@app_views.route('/cities/<city_id>',
-                 strict_slashes=False,
-                 methods=['GET', 'DELETE', 'PUT'])
-def Cities(state_id=None, city_id=None):
-    if state_id is not None:
-        state = storage.get(State, state_id)
-        if state is None:
-            return jsonify({"error": "Not found"}), 404
-
-    if request.method == 'GET':
-        cities = storage.all(City)
-        #print(state.to_dict())
-        print(cities)
-        tab=[]
-        for city in cities:
-            pass
-        return jsonify({"msg": "Cities List all"}), 200
-
-    if request.method == 'POST':
-        data = request.get_json()
-        if type(data) is not dict:
-            return jsonify({"error": "Not a JSON"}), 400
-        if "name" not in data:
-            return jsonify({"error": "Missing name"}), 400
-
-        obj = City(name=data['name'], state_id=state_id)
-
-        storage.new(obj)
-        storage.save()
-
-        return jsonify(obj.to_dict()), 201"""
-
 @app_views.route('/states/<state_id>/cities',
                  strict_slashes=False,
                  methods=['GET'])
@@ -70,6 +35,7 @@ def getCities(state_id):
 def creatCitie(state_id):
     pass
 
+
 @app_views.route('/states/<state_id>/cities',
                  strict_slashes=False,
                  methods=['DELETE'])
@@ -77,10 +43,12 @@ def delCitie(state_id):
     pass
 
 
-@app_views.route('/api/v1/cities/<city_id>',
+@app_views.route('/cities/<city_id>',
                  strict_slashes=False,
                  methods=['GET'])
 def getCitieById(city_id):
     citie = storage.get(City, city_id)
     if citie is None:
         abort(404)
+
+    return jsonify(citie.to_dict()), 200
